b06e49117758107170439cad9d2dabdcae6d2afe
anya bron <anyabron8@gmail.com>
2015-07-26 20:59:28 +0300

    הוספתי עמוד אבל יש בו שגיאות תראה את העברית

--- app/src/main/java/il/co/quix/afeka/hpet/ReportForDog.java
+++ app/src/main/java/il/co/quix/afeka/hpet/ReportForDog.java
@@ -1,8 +1,14 @@
 package il.co.quix.afeka.hpet;
 
+import android.content.ActivityNotFoundException;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -14,12 +20,18 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 
-public class ReportForDog extends MainActivity implements View.OnClickListener {
+public class ReportForDog extends MainActivity implements View.OnClickListener, LocationListener {
 
-    private Button reportBtn;
+    private Button sendReportBtn;
     private Button captureBtn;
     private EditText commentsEdit;
     private ImageView previewImage;
+    private String comment;
+
+    private double latutitude;
+    private double longtitude;
+    double currentLat;
+    double currentLon;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -28,22 +40,23 @@ public class ReportForDog extends MainActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_for_dog);
 
-        reportBtn = (Button) findViewById(R.id.sendReportBtn);
+        sendReportBtn = (Button) findViewById(R.id.sendReportBtn);
         captureBtn = (Button) findViewById(R.id.captureBtn);
         commentsEdit = (EditText) findViewById(R.id.commentsEdit);
         previewImage = (ImageView) findViewById(R.id.previewImage);
 
         captureBtn.setOnClickListener(this);
+        sendReportBtn.setOnClickListener(this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();  // Always call the superclass method first
 
-       /* captureBtn.setText("Blah!");
-        reportBtn.setVisibility(View.INVISIBLE);
-        previewImage.setImageResource(R.mipmap.ic_launcher);*/
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
+       // sendReportBtn.setVisibility(View.INVISIBLE);
     }
 
     @Override
@@ -54,16 +67,20 @@ public class ReportForDog extends MainActivity implements View.OnClickListener {
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             previewImage.setImageBitmap(imageBitmap);
 
-            //TODO get GPS data
+            currentLat = latutitude;
+            currentLon = longtitude;
         }
     }
 
     @Override
     public void onClick(View view) {
-
         if (view.getId() == R.id.captureBtn) {
-
             dispatchTakePictureIntent();
+        }
+        if (view.getId() == R.id.sendReportBtn) {
+             comment = commentsEdit.getText().toString();
+            //TODO sending form
+            commentsEdit.setText(null);
 
         }
     }
@@ -76,49 +93,27 @@ public class ReportForDog extends MainActivity implements View.OnClickListener {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
-    /*
-        @Override
-        public void onPause() {
-            super.onPause();  // Always call the superclass method first
 
-            // Release the Camera because we don't need it when paused
-            // and other activities might need to use it.
-            if (mCamera != null) {
-                mCamera.release();
-                mCamera = null;
-            }
+    private void launchWaze() {
+        try {
+            String url = String.format("waze://?ll=<%s>,<%s>&navigate=yes", String.valueOf(latutitude), String.valueOf(longtitude));
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        } catch (ActivityNotFoundException ex) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.waze"));
+            startActivity(intent);
         }
+    }
+
+
 
         @Override
     public void onResume() {
         super.onResume();  // Always call the superclass method first
+        commentsEdit.setText(comment);
 
-        // Get the Camera instance as the activity achieves full user focus
-        if (mCamera == null) {
-            initializeCamera(); // Local method to handle camera init
-        }
     }
 
-
-    */
-    @Override
-    protected void onStop() {
-        super.onStop();  // Always call the superclass method first
-
-        // Save the note's current draft, because the activity is stopping
-        // and we want to be sure the current note progress isn't lost.
-        ContentValues values = new ContentValues();
-        //values.put(NotePad.Notes.COLUMN_NAME_NOTE, commentsEdit.getText());
-        //values.put(NotePad.Notes.COLUMN_NAME_TITLE,);
-
-    /*getContentResolver().update(
-            mUri,    // The URI for the note to update.
-            values,  // The map of column names and new values to apply to them.
-            null,    // No SELECT criteria are used.
-            null     // No WHERE columns are used.
-    );
-    */
-    }
 
     @Override
     public void onDestroy() {
@@ -126,5 +121,27 @@ public class ReportForDog extends MainActivity implements View.OnClickListener {
 
         // Stop method tracing that the activity started during onCreate()
         android.os.Debug.stopMethodTracing();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        latutitude = location.getLatitude();
+        longtitude = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
--- app/src/main/java/il/co/quix/afeka/hpet/about_us.java
+++ app/src/main/java/il/co/quix/afeka/hpet/about_us.java
@@ -1,17 +1,46 @@
 package il.co.quix.afeka.hpet;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class about_us extends MainActivity {
+public class about_us extends MainActivity implements View.OnClickListener {
+
+    private Button callBtn;
+    private EditText phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        callBtn = (Button) findViewById(R.id.callBtn);
+        phoneNumber = (EditText) findViewById(R.id.phoneNum);
+        callBtn.setOnClickListener(this);
     }
 
-}
+    @Override
+    public void onClick(View view) {
+        // TODO phone call
+        if (view.getId() == R.id.callBtn) {
+            try {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + phoneNumber.getText().toString()));
+                startActivity(callIntent);
+            } catch (ActivityNotFoundException activityException) {
+                Log.e("Calling a Phone Number", "Call failed", activityException);
+            }
+        }
+
+        }
+
+    }
