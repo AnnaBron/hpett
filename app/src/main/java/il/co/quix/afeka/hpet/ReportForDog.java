fc4f330d95bf43928e09e97b924c1dd47dbde3a3
nisomazuz <niso@quix.co.il>
2015-07-29 19:19:11 +0300

    working version without location

--- app/src/main/java/il/co/quix/afeka/hpet/ReportForDog.java
+++ app/src/main/java/il/co/quix/afeka/hpet/ReportForDog.java
@@ -1,37 +1,25 @@
 package il.co.quix.afeka.hpet;
 
-import android.content.ActivityNotFoundException;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
+        import android.content.ContentValues;
+        import android.content.Intent;
+        import android.graphics.Bitmap;
+        import android.provider.MediaStore;
+        import android.support.v7.app.ActionBarActivity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ImageView;
 
 
-public class ReportForDog extends MainActivity implements View.OnClickListener, LocationListener {
+public class ReportForDog extends MainActivity implements View.OnClickListener {
 
-    private Button sendReportBtn;
+    private Button reportBtn;
     private Button captureBtn;
     private EditText commentsEdit;
     private ImageView previewImage;
-    private String comment;
-
-    private double latutitude;
-    private double longtitude;
-    double currentLat;
-    double currentLon;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -40,23 +28,22 @@ public class ReportForDog extends MainActivity implements View.OnClickListener, 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_for_dog);
 
-        sendReportBtn = (Button) findViewById(R.id.sendReportBtn);
+        reportBtn = (Button) findViewById(R.id.sendReportBtn);
         captureBtn = (Button) findViewById(R.id.captureBtn);
         commentsEdit = (EditText) findViewById(R.id.commentsEdit);
         previewImage = (ImageView) findViewById(R.id.previewImage);
 
         captureBtn.setOnClickListener(this);
-        sendReportBtn.setOnClickListener(this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();  // Always call the superclass method first
 
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+       /* captureBtn.setText("Blah!");
+        reportBtn.setVisibility(View.INVISIBLE);
+        previewImage.setImageResource(R.mipmap.ic_launcher);*/
 
-       // sendReportBtn.setVisibility(View.INVISIBLE);
     }
 
     @Override
@@ -67,20 +54,16 @@ public class ReportForDog extends MainActivity implements View.OnClickListener, 
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             previewImage.setImageBitmap(imageBitmap);
 
-            currentLat = latutitude;
-            currentLon = longtitude;
+            //TODO get GPS data
         }
     }
 
     @Override
     public void onClick(View view) {
+
         if (view.getId() == R.id.captureBtn) {
+
             dispatchTakePictureIntent();
-        }
-        if (view.getId() == R.id.sendReportBtn) {
-             comment = commentsEdit.getText().toString();
-            //TODO sending form
-            commentsEdit.setText(null);
 
         }
     }
@@ -93,27 +76,49 @@ public class ReportForDog extends MainActivity implements View.OnClickListener, 
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
+    /*
+        @Override
+        public void onPause() {
+            super.onPause();  // Always call the superclass method first
 
-    private void launchWaze() {
-        try {
-            String url = String.format("waze://?ll=<%s>,<%s>&navigate=yes", String.valueOf(latutitude), String.valueOf(longtitude));
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            startActivity(intent);
-        } catch (ActivityNotFoundException ex) {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.waze"));
-            startActivity(intent);
+            // Release the Camera because we don't need it when paused
+            // and other activities might need to use it.
+            if (mCamera != null) {
+                mCamera.release();
+                mCamera = null;
+            }
         }
-    }
-
-
 
         @Override
     public void onResume() {
         super.onResume();  // Always call the superclass method first
-        commentsEdit.setText(comment);
 
+        // Get the Camera instance as the activity achieves full user focus
+        if (mCamera == null) {
+            initializeCamera(); // Local method to handle camera init
+        }
     }
 
+
+    */
+    @Override
+    protected void onStop() {
+        super.onStop();  // Always call the superclass method first
+
+        // Save the note's current draft, because the activity is stopping
+        // and we want to be sure the current note progress isn't lost.
+        ContentValues values = new ContentValues();
+        //values.put(NotePad.Notes.COLUMN_NAME_NOTE, commentsEdit.getText());
+        //values.put(NotePad.Notes.COLUMN_NAME_TITLE,);
+
+    /*getContentResolver().update(
+            mUri,    // The URI for the note to update.
+            values,  // The map of column names and new values to apply to them.
+            null,    // No SELECT criteria are used.
+            null     // No WHERE columns are used.
+    );
+    */
+    }
 
     @Override
     public void onDestroy() {
@@ -121,27 +126,5 @@ public class ReportForDog extends MainActivity implements View.OnClickListener, 
 
         // Stop method tracing that the activity started during onCreate()
         android.os.Debug.stopMethodTracing();
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-        latutitude = location.getLatitude();
-        longtitude = location.getLongitude();
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
     }
 }
