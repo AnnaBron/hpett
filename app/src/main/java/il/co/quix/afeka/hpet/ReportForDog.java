4ce80abad99b9fe6e07367e36deadfad4a027e87
nisomazuz <niso@quix.co.il>
2015-08-01 14:15:04 +0300

    Add new report works

--- app/src/main/java/il/co/quix/afeka/hpet/ReportForDog.java
+++ app/src/main/java/il/co/quix/afeka/hpet/ReportForDog.java
@@ -1,8 +1,17 @@
 package il.co.quix.afeka.hpet;
 
+        import android.animation.Animator;
+        import android.animation.AnimatorListenerAdapter;
+        import android.annotation.TargetApi;
+        import android.app.AlertDialog;
+        import android.app.LoaderManager;
         import android.content.ContentValues;
         import android.content.Context;
+        import android.content.CursorLoader;
+        import android.content.DialogInterface;
         import android.content.Intent;
+        import android.content.Loader;
+        import android.database.Cursor;
         import android.graphics.Bitmap;
         import android.location.Address;
         import android.location.Criteria;
@@ -10,35 +19,66 @@ package il.co.quix.afeka.hpet;
         import android.location.Location;
         import android.location.LocationListener;
         import android.location.LocationManager;
+        import android.net.Uri;
+        import android.os.AsyncTask;
+        import android.os.Build;
+        import android.provider.ContactsContract;
         import android.provider.MediaStore;
         import android.support.v7.app.ActionBarActivity;
         import android.os.Bundle;
+        import android.util.Base64;
         import android.util.Log;
         import android.view.Menu;
         import android.view.MenuItem;
         import android.view.View;
+        import android.widget.ArrayAdapter;
         import android.widget.Button;
         import android.widget.EditText;
+        import android.widget.ImageButton;
         import android.widget.ImageView;
         import android.widget.TextView;
 
+        import org.apache.http.HttpEntity;
+        import org.apache.http.HttpResponse;
+        import org.apache.http.NameValuePair;
+        import org.apache.http.client.ClientProtocolException;
+        import org.apache.http.client.HttpClient;
+        import org.apache.http.client.entity.UrlEncodedFormEntity;
+        import org.apache.http.client.methods.HttpPost;
+        import org.apache.http.impl.client.DefaultHttpClient;
+        import org.apache.http.message.BasicNameValuePair;
+
+        import java.io.BufferedReader;
+        import java.io.ByteArrayOutputStream;
         import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.io.UnsupportedEncodingException;
+        import java.util.ArrayList;
         import java.util.List;
         import java.util.Locale;
 
+        import static android.util.Base64.*;
 
-public class ReportForDog extends MainActivity implements View.OnClickListener,LocationListener {
+
+public class ReportForDog extends MainActivity implements View.OnClickListener,LocationListener,LoaderManager.LoaderCallbacks<Cursor> {
 
     private Button reportBtn;
-    private Button captureBtn;
+    private ImageButton captureBtn;
     private EditText commentsEdit;
     private ImageView previewImage;
+    private String imageStr = null;
     protected LocationManager locationManager;
     protected LocationListener locationListener;
     protected Context context;
+    private AddReportTask addDogTask = null;
+    private View mProgressView;
+    private View mReportFormView;
+    private AlertDialog.Builder alertDialogBuilder;
     Double lat;
     Double lng;
     String address;
+    String notes;
     TextView addressView;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -49,20 +89,83 @@ public class ReportForDog extends MainActivity implements View.OnClickListener,L
         setContentView(R.layout.activity_report_for_dog);
 
         reportBtn = (Button) findViewById(R.id.sendReportBtn);
-        captureBtn = (Button) findViewById(R.id.captureBtn);
+        captureBtn = (ImageButton) findViewById(R.id.captureBtn);
         commentsEdit = (EditText) findViewById(R.id.commentsEdit);
         previewImage = (ImageView) findViewById(R.id.previewImage);
+        mReportFormView =  (View) findViewById(R.id.reports_form);
+        mProgressView =  (View) findViewById(R.id.report_progress);
 
+        context = ReportForDog.this;
+        alertDialogBuilder = new AlertDialog.Builder(context);
         captureBtn.setOnClickListener(this);
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         List<String> providers  = locationManager.getProviders(true);
 
+
+        reportBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendForm();
+            }
+        });
+
         // Log.d("Latitude", "disable");
 
     }
 
+    private boolean validateForm() {
+        if ( !locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            alertMessage("אנא הפעל את שרותי ה gps במכשיר כדי לשלוח דיווח");
+            return false;
+        }
+        if ( imageStr == null) {
+            alertMessage("לא צולמה תמונה של הכלב המדווח");
+            return false;
+        }
+        if ( address == null) {
+            alertMessage("כתובת הדיווח טרם זוהתה");
+            return false;
+        }
+        return true;
+    }
+
+    private void alertMessage(String msg) {
+        new AlertDialog.Builder(context)
+                .setTitle("שגיאה בטופס")
+                .setMessage(msg)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+
+
+    private void sendForm() {
+        if (addDogTask != null) {
+            return;
+        }
+        if(!validateForm()){
+            // create alert dialog
+
+            return;
+        }
+
+
+       showProgress(true);
+       addDogTask = new AddReportTask(imageStr, address ,commentsEdit.getText().toString(),lat,lng);
+       addDogTask.execute((Void) null);
+
+
+
+
+        }
 
 
     @Override
@@ -82,6 +185,11 @@ public class ReportForDog extends MainActivity implements View.OnClickListener,L
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             previewImage.setImageBitmap(imageBitmap);
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 90, stream); //compress to which format you want.
+            byte [] byte_arr = stream.toByteArray();
+            imageStr = Base64.encodeToString(byte_arr,Base64.DEFAULT);
 
             //TODO get GPS data
         }
@@ -202,4 +310,190 @@ public class ReportForDog extends MainActivity implements View.OnClickListener,L
         }
         return null;
     }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    public void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mReportFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mReportFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mReportFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mReportFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this,
+                // Retrieve data rows for the device user's 'profile' contact.
+                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
+                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
+
+                // Select only email addresses.
+                ContactsContract.Contacts.Data.MIMETYPE +
+                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
+                .CONTENT_ITEM_TYPE},
+
+                // Show primary email addresses first. Note that there won't be
+                // a primary email address if the user hasn't specified one.
+                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        List<String> emails = new ArrayList<String>();
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            emails.add(cursor.getString(ProfileQuery.ADDRESS));
+            cursor.moveToNext();
+        }
+
+        // addEmailsToAutoComplete(emails);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+
+    }
+
+    private interface ProfileQuery {
+        String[] PROJECTION = {
+                ContactsContract.CommonDataKinds.Email.ADDRESS,
+                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
+        };
+
+        int ADDRESS = 0;
+        int IS_PRIMARY = 1;
+    }
+
+
+
+
+    public class AddReportTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String image;
+        private final String address;
+        private final String notes;
+        private final String lat;
+        private final String lng;
+
+        AddReportTask(String image, String address, String notes,Double lat, Double lng) {
+            this.image = image;
+            this.address = address;
+            this.notes = notes;
+            this.lat = String.valueOf(lat);
+            this.lng = String.valueOf(lng);
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+            String s="";
+            try {
+
+                HttpClient httpClient=new DefaultHttpClient();
+
+                HttpPost httpPost=new HttpPost("http://hpet.quix.co.il/api/addreport");
+
+                List<NameValuePair> list=new ArrayList<NameValuePair>();
+                list.add(new BasicNameValuePair("description", notes));
+                list.add(new BasicNameValuePair("lat",lat));
+                list.add(new BasicNameValuePair("lng",lng));
+                list.add(new BasicNameValuePair("photo_stream",image));
+                list.add(new BasicNameValuePair("address",address));
+                UrlEncodedFormEntity form = new UrlEncodedFormEntity(list,"UTF-8");
+                httpPost.setEntity(form);
+                HttpResponse httpResponse=  httpClient.execute(httpPost);
+
+                HttpEntity httpEntity=httpResponse.getEntity();
+                Log.d("DOG",readResponse(httpResponse));
+                //  ReportForDog.this.setLoginStatus(Integer.parseInt(readResponse(httpResponse)) > 0 ? true : false);
+
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return true;
+
+/*            for (String credential : DUMMY_CREDENTIALS) {
+                String[] pieces = credential.split(":");
+                if (pieces[0].equals(mEmail)) {
+                    // Account exists, return true if the password matches.
+                    return pieces[1].equals(mPassword);
+                }
+            }*/
+
+            // TODO: register the new account here.
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            addDogTask = null;
+            showProgress(false);
+
+            Intent i = new Intent(getApplicationContext(), Volenteer.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            // Add new Flag to start new Activity
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i);
+            finish();
+        }
+
+        @Override
+        protected void onCancelled() {
+            addDogTask = null;
+            showProgress(false);
+        }
+
+        public String readResponse(HttpResponse res) {
+            InputStream is=null;
+            String return_text="";
+            try {
+                is=res.getEntity().getContent();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(is));
+                String line="";
+                StringBuffer sb=new StringBuffer();
+                while ((line=bufferedReader.readLine())!=null)
+                {
+                    sb.append(line);
+                }
+                return_text=sb.toString();
+            } catch (Exception e)
+            {
+
+            }
+            return return_text;
+
+        }
+    }
 }
+
